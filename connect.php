f19e9df603e5b5a9e0eff3cd76651ba8226f0eb7
Valmir Santana <valmirsantanape@gmail.com>
2020-05-24 20:30:57 -0300

    Initial commit

---
+++ connect.php
@@ -0,0 +1,15 @@
+﻿<?php
+//Arquivo de conexao com o banco de dados
+$host = "localhost";
+$user = "root";
+$pass = "";
+$database = "bd_curso";
+
+
+
+$link = mysqli_connect($host,$user,$pass,$database);
+if($link==true)
+	echo"Ok";
+else
+	echo"Falha na conecção";
+?>
