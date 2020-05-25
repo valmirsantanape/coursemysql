f035f75931d45b60ce7dbe2492c6e3b8af56db32
Valmir Santana <valmirsantanape@gmail.com>
2020-05-24 21:03:37 -0300

    exibindo informacoes do banco de dados
    Atualizado

--- index.php
+++ index.php
@@ -4,5 +4,13 @@ echo"<h1>Dados dos Alunos</h1>";
 
 //Exibir dados do banco de dados
 
-$sql = mysqli_query($link, "SELECT * FROM tb_alunos");
+$sql = mysqli_query($link, "select * from tb_aluno");
+while($dados = mysqli_fetch_array($sql)){
+	echo "$dados[id_aluno]<br>";
+	echo "$dados[nome]<br>";
+	echo "$dados[email]<br>";
+	echo "$dados[senha]<hr>";
+}
+// a função mysqli_fetch_array faz uma varredura da vareavel usada como paramentro
+//$dados vai receber o conteudo armazenado no banco de dados
 ?>
